79ced345ee286297cde47de1cb1b77466906dad6
huggy <6511667+makeding@users.noreply.github.com>
2021-05-02 18:56:03 +0800

    *.js -> *

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 yarn build
 # vuepress will redirect automanually x to x.html
 # to disable redirect will install another dependencies
-find dist -type f -name "*.js" -print0 | xargs -0 sed -i 's/"\.html"/"\.html1"/g'
-find dist -type f -name "*.js" -print0 | xargs -0 sed -i 's/\.html"/"/g'
-find dist -type f -name "*.js" -print0 | xargs -0 sed -i 's/"\.html1"/"\.html"/g'
+find dist -type f -print0 | xargs -0 sed -i 's/"\.html"/"\.html1"/g'
+find dist -type f -print0 | xargs -0 sed -i 's/\.html"/"/g'
+find dist -type f -print0 | xargs -0 sed -i 's/"\.html1"/"\.html"/g'
