8f68ba42dcfead7f682c4a4936eefdb6f545c85b
huggy <i@huggy.moe>
2025-03-23 16:00:58 +0900

    deps update

--- config_sample.js
+++ config_sample.js
@@ -1,27 +1,27 @@
 export default {
     mongodb: {
-        "uri": "mongodb://127.0.0.1:27017",
-        "dbname": "pixiv_bot"
+        uri: "mongodb://127.0.0.1:27017",
+        dbname: "pixiv_bot"
     },
     pixiv: {
-        "cookie": "",
-        "ua": "",
-        "pximgproxy": "",
-        "csrf": "",
-        "ugoiraurl": ""
+        cookie: "",
+        ua: "",
+        pximgproxy: "",
+        csrf: "",
+        ugoiraurl: ""
     },
     tg: {
-        "master_id": 1,
-        "token": "",
-        "access_token": "",
-        "salt": "",
-        "refetch_api": ""
+        master_id: 1,
+        token: "",
+        access_token: "",
+        salt: "",
+        refetch_api: ""
     },
     web: {
-        "enabled": false,
-        "host": "127.0.0.1",
-        "port": 30005,
-        "recaptcha_key": false,
-        "hcaptcha_key": false
+        enabled: false,
+        host: "127.0.0.1",
+        port: 30005,
+        recaptcha_key: false,
+        hcaptcha_key: false
     }
 }
