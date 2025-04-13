88d3be3bd0da64be9086a088ddb5b897479d13f7
huggy <i@huggy.moe>
2025-04-13 18:42:16 +0900

    fix wrong format template

--- handlers/telegram/df.js
+++ handlers/telegram/df.js
@@ -6,11 +6,12 @@ export default {
         message: '%\\#NSFW |NSFW%%\\#AI |AI%[%title%](%url%) / [%author_name%](%author_url%)% |p%'
             + '%\n|tags%'
             + '%\n>|description%',
+        inline: '%\\#NSFW |NSFW%%\\#AI |AI%[%title%](%url%) / [%author_name%](%author_url%)% |p%'
+            + '%\n|tags%'
+            + '%\n>|description%',
         // single caption
         mediagroup_message: '[%mid| %%title%% |p%](%url%)'
-            + '%\n|tags%',
-        inline: '%\\#NSFW |NSFW%%AI |AI%[%title%](%url%) / [%author_name%](%author_url%)% |p%'
             + '%\n|tags%'
-            + '%\n>|description%'
+            + '%\n>|description%',
     }
 }
